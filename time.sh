0fdde1ac7bd08df39e146a94934c9bb68bc5c320
Cloud User <centos@ip-10-0-1-188.ca-central-1.compute.internal>
2023-02-01 00:28:57 +0000

    update

---
+++ time.sh
@@ -0,0 +1,15 @@
+---
+echo `date`
+
+- year=`date +%y`
+month=`date +%m`
+day=`date +%d`
+
+- hour=`date +%H`
+minute=`date +%M`
+second=`date +%S`
+
+- echo " B is for Boss"
+echo "current Date is : $day/$month/$year"
+echo "current Time is : $hour:$minute:$second"
+...
